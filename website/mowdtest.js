d67416a6052d3ba22197b3029dd28552135d6ea1
Mowd Chen <mowd8574@gmail.com>
2013-11-02 18:52:40 +0800

    Add test data for testing Chinese column.

---
+++ website/mowdtest.js
@@ -0,0 +1,50 @@
+{
+  "properties": {
+           "Test Array" : {
+               "valueType": "item"
+           }
+       },
+  "items":[
+    {
+      "label": "aaaaa",
+        "url": "http://data.gov.tw/opendata/Details?sno=A05000000C-00011",
+      "Category": "就醫",
+      "TXT": "http://www.mnd.gov.tw/Publish.aspx?cnid=3393&p=57365",
+      "Title": "衛教資訊",
+      "Test Array": ["aaaaa","bbbbb","ccccc"],
+      "主要欄位說明": "腸病毒防治、防治憂鬱症、消弭酒駕、認識腦膜炎、蛇咬傷知預防及處理、豪雨過後防疫工作、心因性休克、預防猝死、預防代謝症候群、新型冠狀病毒、流行性腦脊隨膜炎、流感疫苗接種具體作法、登革熱防治、憂鬱與自殺診治、肺結核簡介、運動傷害之預防及治療。",
+      "授權方式": "政府資料開放平臺資料使用規範",
+      "授權說明網址": "http://data.gov.tw/opendata/principle",
+      "收錄期間": "民國101年7月01日~民國101年12月31日",
+      "更新頻率": "每6個月",
+      "編碼格式": "UTF-8",
+      "計費方式": "免費",
+      "資料量": "約8筆",
+      "資料集描述": "當前流行傳染病預防及國人常見生理、心理疾病的防範與治療。",
+      "資料集提供機關": "國防部",
+      "資料集提供機關聯絡人": "莊小姐",
+      "資料集提供機關聯絡人電話": "02-23811593"
+    },
+    {
+      "label": "bbbbb",
+  "url": "http://data.gov.tw/opendata/Details?sno=301000000A-00001",
+      "CSV": "http://tgos.nat.gov.tw/tgos/Web/MAPData/Apply/TGOS_Apply_FreeDownload.aspx?ID=9",
+      "Category": "生活安全及品質",
+      "Title": "警察局及所屬分駐所派出所資料",
+      "Test Array": ["bbbbb","ccccc"],
+      "主要欄位說明": "派出所名稱、派出所地址、電話、點位資料(x軸y軸)、點位檔",
+      "授權方式": "政府資料開放平臺資料使用規範",
+      "授權說明網址": "http://data.gov.tw/opendata/principle",
+      "收錄期間": "民國98年5月05日~民國101年6月06日",
+      "更新頻率": "半年",
+      "編碼格式": "BIG-5",
+      "計費方式": "免費",
+      "資料量": "約1817筆",
+      "資料集描述": "警政署各縣(市)警察(分)局暨所屬分駐(派出)所點資料，座標系統採TWD97經緯度",
+      "資料集提供機關": "內政部",
+      "資料集提供機關聯絡人": "張先生",
+      "資料集提供機關聯絡人電話": "02-23931791#6046",
+      "資料集說明網址": "http://tgos.nat.gov.tw/tgos/Web/MAPData/Apply/TGOS_Apply_FreeDownload.aspx?ID=9"
+    }
+  ]
+}
