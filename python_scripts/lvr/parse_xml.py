ccf13267f55419a3359dff225dfbba083936da7c
Chandler <previa@gmail.com>
2013-11-03 09:40:09 +0800

    Update house_buy data

--- python_scripts/lvr/parse_xml.py
+++ python_scripts/lvr/parse_xml.py
@@ -2,22 +2,42 @@
 # -*- encoding: utf8 -*-
 # coding=UTF-8
 
+import json
 import sys
 import optparse
 import xml.etree.cElementTree as ET
 
 
-__author__ = 'fcamel'
 
+from os import listdir
+from os.path import isfile, join
+
+__author__ = 'fcamel'
 
 def main(args):
     '''\
     %prog [options]
     '''
-    tree = ET.ElementTree(file='H_lvr_land_A.XML')
+    result_list = []
+    mypath="/Users/chandler/workspace/YahooHackDay2013/python_scripts/lvr"
+    onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+    for filename in onlyfiles:
+        if "A_lvr_land_A.XML" not in filename: continue
+        #print filename
+        result_list.append(get_data_from_file(filename))
+    #print result_list
+    result = {}
+    result["items"] = result_list
+    result_2 = json.dumps(result, ensure_ascii=False)
+    print result_2
+    return 0
+
+
+def get_data_from_file(filename):
+    result_list = []
+    tree = ET.ElementTree(file=filename)
     root = tree.getroot()
     #print root
-    result_list = []
     #print root.tag, root.attrib
     for child_of_root in root:
         #print child_of_root.tag, child_of_root.text
@@ -32,11 +52,7 @@ def main(args):
             text = str(child_of_child.text.encode('utf-8'))
             tmp_dict[tag] = text
         result_list.append(tmp_dict)
-    result = {}
-    result["items"] = return_list
-    result_2 = json.dumps(result, ensure_ascii=False)
-    print result_2.encode('utf-8')
-    return 0
+    return result_list
 
 
 if __name__ == '__main__':
